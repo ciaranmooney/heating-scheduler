e98f41be7e809758d735d4fb97810b60f02d41af
Ciarán Mooney <general.mooney@googlemail.com>
2017-10-05 20:30:53 +0100

    heating-schedule.py - fleshed out schedule.xml - added extra event in one day

--- heating-schedule.py
+++ heating-schedule.py
@@ -6,15 +6,54 @@
 # A heating controller for my home. It currently uses a hard coded XML file to
 # program the schedule. It issues commands via I2C to an arduino that controls
 # the boiler over 433 Mhz RF.
-
+#
 DEBUG = True
 SCHEDULE = 'schedule.xml'
+
+from datetime import datetime
+import xml.etree.ElementTree as ET
 
 def log(message, debug):
     ''' Handy global for printing debug messages.
     '''
     if debug:
         print(message)
+
+class event(object):
+    '''
+    '''
+
+    def __init__(self):
+        '''
+        '''
+        pass
+
+def checkSchedule(now, schedule):
+    ''' Checks the schedule to see if heaing is required and if so what
+        temperature.
+    '''
+    dotw = datetime.weekday(now)
+    time = datetime.time(now)
+    
+    tree = ET.parse('schedule')
+    root = tree.getroot()
+    events = root[dotw]
+    
+    return heat, temperature
+
+def checkTemp():
+    '''
+    '''
+    pass
+
+def heatingOn():
+    '''
+    '''
+
+def heatingOff():
+    '''
+    '''
+    pass
 
 def run(schedule, debug):
     ''' Main loop which checks to see if a command needs to be issued.
@@ -24,15 +63,16 @@ def run(schedule, debug):
     # action required
     # wait
     while True:
-        heatingStatus, requiredTemp = checkSchedule(now)
+        now = datetime.now
+        heatingRequired, requiredTemp = checkSchedule(now, schedule)
         currentTemp = checkTemp()
-        if heatingStatus and (currentTemp < requiredTemp):
+        if heatingRequired and (currentTemp < requiredTemp):
             log("Too cool, turning heating on", debug)
             heatingOn()
-        if heatingStatus and (currentTemp > requiredTemp):
-            log("Too hot, turning heating on", debug)
+        if heatingRequired and (currentTemp > requiredTemp):
+            log("Too hot, turning heating off", debug)
             heatingOff()
-        if heatingStatus == False:
+        if heatingRequired == False:
             log("Heating off", debug)
             heatingOff()
         
